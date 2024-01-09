d106a40b204c36c29a80762f463c59214423a70f
LorenzoSpera <lorenzospera2@gmail.com>
2024-01-09 10:36:07 +0100

    Updated

--- run_sciame.py
+++ run_sciame.py
@@ -13,18 +13,17 @@
 # restituisce le grandezze richieste in una tabella e    #
 # l'utente può decidere o meno se visualizzare i risulta #
 # ti dal punto di vista grafico. Nello script è presente #
-# una simulazione per il materiale acqua e uno per il    #
-# materiale silicato di bismuto. L'utente può cambiare   #
-# liberamente i valori ed eseguire varie simulazioni.    #
-# Nella parte finale si trova il confronto per lo        #
-# sviluppo dello sciame a partire da diversi valori      #
-# dell'energia iniziale                                  #
+# una simulazione per il materiale acqua e per il silica #
+# to di bismuto e una con dei valori diversi. L'utente   #
+# può cambiare liberamente i valori ed eseguire varie    #
+# simulazioni. Nella parte finale si trova il confronto  #
+# per lo sviluppo dello sciame a partire da diversi      #
+# valori dell'energia iniziale.                           #
 ##########################################################
 import numpy as np 
 import sys, os 
-import matplotlib.pyplot as plt
 
-sys.path.append('/Users/lorenzospera/Desktop/Terzo_anno/MCF/github/Progetto-Finale-MCF/sciame_def.py')  # inserire il path del file sciame_def.py
+sys.path.append('//Users/lorenzospera/Desktop/Terzo_anno/MCF/github/Progetto-Finale-MCF/sciame_def.py')  # inserire il path del file sciame_def.py
 
 # importare il modulo
 import sciame_def
@@ -39,17 +38,17 @@ Valori di partenza scelti dall'utente:
     - passo di avanzamento (compreso tra 0 e 1)
 
     N.B = per ottimizzare la simulazione si consiglia di utilizzare dei valori per l'energia iniziale sufficientemente alti,
-    ovvero dalle centinaia di MeV. Si tenga poi conto del fatto che la perdita di energia è proporzionale al passo di avanzamento s
+    ovvero dalle centinaia o migliaglia di MeV. Si tenga poi conto del fatto che la perdita di energia è proporzionale al passo di avanzamento s
     ; scegliendo s grande (sempre tra 0 e 1 ) aumenta la probabilità di produzione di coppia e di emissione (e quindi il numero di 
-    particelle), ma, essendo la perdita di energia proporzionale ad s, questa sarà altrettanto grande e la simulazione 
-    si arresta più velocemente.
+    particelle), ma, essendo la perdita di energia proporzionale ad s, questa sarà altrettanto grande e la simulazione potrebbe
+    arrestarsi più velocemente.
 """
 
-tipo_particella_iniziale  = 'Elettrone'     # selezionare il tipo di particella iniziale: 'Elettrone', 'Positrone', 'Fotone'
-energia_utente = 50000                      # energia iniziale scelta dall'utente in MeV
-passo_utente = 0.2                          # passo di avanzamento scelto dall'utente
-lunghezza_radiazione_h2O = 36.08            # in cm 
-lunghezza_radiazione_bso = 1.097            # in cm 
+tipo_particella_iniziale  = 'Elettrone'       # selezionare il tipo di particella iniziale: 'Elettrone', 'Positrone', 'Fotone'
+energia_utente = 600000                       # energia iniziale scelta dall'utente in MeV
+passo_utente = 0.33                           # passo di avanzamento scelto dall'utente
+lunghezza_radiazione_h2O = 36.08              # in cm 
+lunghezza_radiazione_bso = 7.81               # in cm (1.097)
 
 perdita_per_ionizzazione_h2O = energia_utente/(np.e*lunghezza_radiazione_h2O)     # in MeV/cm
 perdita_per_ionizzazione_bso = energia_utente/(np.e*lunghezza_radiazione_bso)     # in Mev/cm
@@ -61,7 +60,17 @@ energia_critica_elettroni_bso = 10.68   # in MeV
 energia_critica_positroni_bso = 10.32   # in Mev 
 
 
-# simulazione di una particella (tipo_particella) con energia E0 che genera lo sciame
+# simulazione di una particella (tipo_particella) con energia E0 che genera lo sciame con parametri scelti dall'utente e quelli dei due materiali + simulazione aggiuntiva
+
+"""
+Per la simulazione sciame utente, si è scelto un valore della perdita di energia molto più basso rispetto a quello dei materiali per 
+effettuare un confronto con questi. In ogni caso, essendo anche il passo relativamente piccolo lo sviluppo dello sciame richiede 
+più tempo rispetto alle altre simulazioni. Una volta verificato il funzionamento tale simulazione può essere commentata per un'analisi 
+dei risultati più veloce o, in maniera alternativa, si possono cambiare liberamente i parametri.
+"""
+sciame_utente = sciame_elettromagnetico(tipo_particella = tipo_particella_iniziale, E0 = energia_utente, Ec_elettroni=energia_critica_elettroni_bso
+                                        ,Ec_positroni=energia_critica_positroni_bso
+                                        ,dEx0 = energia_utente/3500, s = passo_utente, X0 = lunghezza_radiazione_h2O)
 
 sciame1_bso  = sciame_elettromagnetico(tipo_particella = tipo_particella_iniziale, E0 = energia_utente, Ec_elettroni=energia_critica_elettroni_bso
                                         ,Ec_positroni=energia_critica_positroni_bso
@@ -71,12 +80,17 @@ sciame1_h2O = sciame_elettromagnetico(tipo_particella = tipo_particella_iniziale
                                       ,Ec_positroni=energia_critica_positroni_h2O
                                       ,dEx0 = perdita_per_ionizzazione_h2O, s = passo_utente, X0 = lunghezza_radiazione_h2O )
 
-print("Simulazione per valori scelti dall'utente usando valori critici di H_2O ")
+print("Simulazione per valori arbitrari scelti dall'utente. La simulazione potrebbe richiedere un pò di tempo: ")
+
+sciame_utente.simulazione_sciame()
+
+print('------------------------------------------------------------------------')
+print("Simulazione per valori scelti dall'utente usando valori critici di H_2O. La simulazione potrebbe richiedere un pò di tempo: ")
 
 sciame1_h2O.simulazione_sciame()
 
 print('------------------------------------------------------------------------')
-print("Simulazione per valori scelti dall'utente usando valori critici di BSO ")
+print("Simulazione per valori scelti dall'utente usando valori critici di BSO. la simulazione potrebbe richiedere un pò di tempo: ")
 
 sciame1_bso.simulazione_sciame()
 
@@ -92,10 +106,10 @@ materiali. Come esempio viene implementata la seguente simulazione con alcuni va
 L'utente può cambiare liberamente i valori e vedere come variano i risultati.
 """
 
-energia_0 = 20000
-energia_1 = 200000              # queste sono le 3 energie iniziali che l'utente può scegliere
-energia_2 = 2000000
-passo_utente_2 = 0.3            # passo della simulazione scelto dall'utente
+energia_0 = 600000
+energia_1 = 700000              # queste sono le 3 energie iniziali che l'utente può scegliere
+energia_2 = 800000
+passo_utente_2 = 0.55           # passo della simulazione scelto dall'utente
 
 tipo_particella_2 = 'Elettrone' # selezionare il tipo di particella iniziale: 'Elettrone', 'Positrone', 'Fotone'
 
@@ -115,6 +129,14 @@ if (confronto_sciame==1):
                                             s = passo_utente_2, X0_h20 = lunghezza_radiazione_h2O, X0_bso = lunghezza_radiazione_bso)
 else:
     print("Simulazione terminata.")
+
+
+
+
+
+
+
+
 
 
 
